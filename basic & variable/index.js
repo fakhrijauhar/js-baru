fbfd9bb073abbb68d7850ad1a1c68b93d5cff032
fakhrijauhar <aljamshitt@gmail.com>
2024-12-03 21:44:03 +0700

    basic & variable

---
+++ basic & variable/index.js
@@ -0,0 +1,19 @@
+//basic variable
+//ada 3 macam variable :
+var name = 'baben'
+let nome = 'joni'
+const neme = 'jene'
+/* 3 variable mempunya kebedaan masing masing :
+1.var : bisa diakses di luar scope dan bisa di ubah nialinya
+2.let : bisa di akses hanya di dalem scope dan bisa di ubah nilainya
+3.const : bisa di akses hanya di dalem scop dan tidak bisa di ubah nilainya
+
+minus var gampang terkena bug karena bisa di akses di mana aja
+*/
+
+const nama = 'fakhri'
+console.log('nama saya '+ nama)
+let age = '19'
+console.log('umur saya '+ age)
+let job = 'enginer'
+console.log('pekerjaan saya '+ job)
