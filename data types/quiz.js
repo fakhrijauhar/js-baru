ce2d69a74615a597374d4dfda0ed0ddc92b5a624
fakhrijauhar <aljamshitt@gmail.com>
2024-12-03 22:55:56 +0700

    first quizz

---
+++ data types/quiz.js
@@ -0,0 +1,10 @@
+const nama = 'fakhri '
+const ttd = 2005
+let hobby = 'maen bola'
+let tinggal = 'tanggerang selatan '
+
+const ubahTtd = 2024
+const umur = ubahTtd - ttd
+
+const perkenalan = `halo nama saya ${nama}saya umur ${umur} hobby saya ${hobby} saya tinggal di ${tinggal}`
+console.log(perkenalan)
