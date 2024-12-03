1f36b7c8c880b929acf104f85626f904a742cfd6
fakhrijauhar <aljamshitt@gmail.com>
2024-12-03 23:22:27 +0700

    boolean

---
+++ data types/boolean.js
@@ -0,0 +1,27 @@
+/*boolean itu cuman ada true or false
+*/
+
+//contoh 1 boolean
+let flag = false
+
+const name1 = 'fakhri'
+const name2 = 'fakhri'
+
+if(name1 === name2){
+    flag = true
+    if(flag === true){
+        console.log(`selamat ${name1} and keterima`)
+    }
+}else{
+    flag = false
+    console.log('maaf anda tidak keterima')
+}
+
+//contoh 2 boolean math
+
+let y = 7
+let x = 7
+let z = 5
+
+console.log(x === z)//false
+console.log(y === x)//true
